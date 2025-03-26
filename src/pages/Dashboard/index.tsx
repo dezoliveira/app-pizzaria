0877c23d56e3edf18607017b245eff455dc42739
Andres Oliveira <andresoliveira@protonmail.com>
2025-03-26 14:53:24 -0300

    dashboard structure

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React = require("react")
+import { View, Text } from 'react-native'
+
+export default function Dashboard(){
+  return (
+    <View>
+      <Text>Tela Dashboard</Text>
+    </View>
+  )
+}
