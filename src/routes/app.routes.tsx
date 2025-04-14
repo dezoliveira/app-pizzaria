29377acc47a08e33eb2bb3daec5dc493eb77e8ff
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-14 16:39:25 -0300

    get Param and redirect to order screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Dashboard from "../pages/Dashboard";
+import Order from "../pages/Order/order";
 
-const Stack = createNativeStackNavigator()
+export type StackParamsList = {
+  Dashboard: undefined,
+  Order: {
+    number: number | string
+    order_id: string
+  }
+}
+
+
+const Stack = createNativeStackNavigator<StackParamsList>()
 
 function AppRoutes() {
   return (
@@ -12,6 +22,12 @@ function AppRoutes() {
         name="Dashboard"
         component={Dashboard}
         options={{ headerShown : false }}
+      />
+
+      <Stack.Screen
+        name="Order"
+        component={Order}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   )
