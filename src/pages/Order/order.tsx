df12effa3bd80bfb3d3d2b121ea5314975b6c840
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-16 14:38:22 -0300

    add close order functionallity

--- src/pages/Order/order.tsx
+++ src/pages/Order/order.tsx
@@ -7,9 +7,11 @@ import {
   TextInput
 } from "react-native";
 
-import { useRoute, RouteProp } from '@react-navigation/native'
+import { useRoute, RouteProp, useNavigation } from '@react-navigation/native'
 
 import { Feather } from '@expo/vector-icons'
+
+import { api } from '../../services/api'
 
 type RouteDetailParams = {
   Order: {
@@ -22,12 +24,29 @@ type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
 
 export default function Order() {
   const route = useRoute<OrderRouteProps>()
+  const navigation = useNavigation()
+
+  async function handleCloseOrder() {
+    try {
+      
+      await api.delete('/order', {
+        params: {
+          order_id: route.params?.order_id
+        }
+      })
+
+      navigation.goBack()
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Mesa {route.params.number}</Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleCloseOrder}>
           <Feather name="trash-2" size={28} color="#ff3f4b" />
         </TouchableOpacity>
       </View>
