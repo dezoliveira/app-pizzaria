1e0800ac514fa6f9b06cfb7c112e8c90d069dedb
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-17 12:08:53 -0300

    adding item to order

--- src/pages/Order/order.tsx
+++ src/pages/Order/order.tsx
@@ -112,13 +112,32 @@ export default function Order() {
     setProductSelected(item)
   }
 
+  async function handleAddItem() {
+    const response = await api.post('/order/add', {
+      order_id: route.params.order_id,
+      product_id: productSelected?.id,
+      amount: Number(amount)
+    })
+
+    let data = {
+      id: response.data.id,
+      product_id: productSelected?.id as string,
+      name: productSelected?.name as string,
+      amount: amount
+    }
+
+    setItems(oldArray => [...oldArray, data])
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Mesa {route.params.number}</Text>
-        <TouchableOpacity onPress={handleCloseOrder}>
-          <Feather name="trash-2" size={28} color="#ff3f4b" />
-        </TouchableOpacity>
+        {items.length === 0 && (
+          <TouchableOpacity onPress={handleCloseOrder}>
+            <Feather name="trash-2" size={28} color="#ff3f4b" />
+          </TouchableOpacity>
+        )}
       </View>
 
       {category.length !== 0 && (
@@ -149,7 +168,7 @@ export default function Order() {
       </View>
 
       <View style={styles.actions}>
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAddItem}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
 
